4b92e52bd20ac5cfa37527e625a6eaa7b838c306
ULin8787 <102144270+ULin8787@users.noreply.github.com>
2022-03-22 16:43:07 +0800

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,38 @@
+class Menu extends Phaser.Scene{
+    constructor(){
+        super({ key: "Menu" });
+        this.button=null;
+        this.particles=null;
+        this.emitter=null;
+    }
+    
+    preload()
+    {
+        this.load.image("button","PNG/simple/24.png");
+        this.load.image("heart","assets/heart.png");
+    }
+    create()
+    {
+        this.button=this.add.image(450,250,"button").setInteractive({ useHandCursor:true }).on("pointerup", () => {this.startgame();});
+        this.gamestart=this.add.text(400,250,"start",
+        {
+            fontSize:"32px",
+            color: "#ffffff"
+        });
+
+        this.particles=this.add.particles('heart');
+        this.emitter=this.particles.createEmitter();    
+        this.emitter.setPosition(450,200);
+        this.emitter.setSpeed(200);        
+        this.emitter.setBlendMode(Phaser.BlendModes.ADD);
+    }
+
+    startgame()
+    {
+        this.scene.start("Scene");
+    }
+
+    update(){
+
+    }
+}
